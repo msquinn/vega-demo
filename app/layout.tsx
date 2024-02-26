e738ad4758cc9d19b83464050796656c13e7d6c2
Matt <mattshowelquinn@gmail.com>
2024-02-26 14:48:06 -0600

    feat: fix open graph images

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,12 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Sankey Diagram",
   description: "Created by Matt Quinn. A Sankey Diagram to visualize the flow of data.",
+  creator: "Matt Quinn",
+  keywords: ["Sankey", "Diagram", "Vega", "Vega Lite", "Next.js", "React"],
+  metadataBase: new URL("https://vega-demo.netlify.app"),
+  openGraph:{
+    images: '/opengraph-image.png',
+  }
 };
 
 export default function RootLayout({
